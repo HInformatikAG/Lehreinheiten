45211e1d0b45e0aa5b0de2a7be9543aa6c3c7a9b
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2019-08-13 16:55:02 +0200

    Tetriscode aus den Lehreinheiten gelöscht;

--- src/com/hoffrogge/tetris/AllgemeinerTetromino.java
+++
@@ -1,6 +0,0 @@
-package com.hoffrogge.tetris;
-
-//in diese Klasse können Methoden rein, die bei allen Tetrominos identisch sind, dann muss nicht jede Klasse sie implementieren
-public abstract class AllgemeinerTetromino implements TetrominoSpielstein {
-
-}
