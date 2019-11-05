6e3bc1e3b072f9c50f8e981b1aa3e719a5c9433b
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2019-11-05 11:44:18 +0100

    Übungsaufgaben zu Enum, Interface und Collection

---
+++ src/com/hoffrogge/uebungen/FarbenInfosImpl.java
@@ -0,0 +1,33 @@
+package com.hoffrogge.uebungen;
+
+import java.util.Collection;
+
+public class FarbenInfosImpl implements FarbenInfos {
+
+    /* Aufgabe 1: Implementiere alle Methoden des Interfaces "FarbenInfos" */
+
+    /*
+     * Aufgabe 2: Führe die Testklasse "FarbenInfosTest" aus, die Aufgabe ist
+     * erledigt, wenn alle Tests grün laufen.
+     */
+
+    @Override
+    public int getRotAnteil(Farbe farbe) {
+        throw new UnsupportedOperationException("Implementiere mich!");
+    }
+
+    @Override
+    public int getGruenAnteil(Farbe farbe) {
+        throw new UnsupportedOperationException("Implementiere mich!");
+    }
+
+    @Override
+    public int getBlauAnteil(Farbe farbe) {
+        throw new UnsupportedOperationException("Implementiere mich!");
+    }
+
+    @Override
+    public Collection<Farbe> getAlleFarben() {
+        throw new UnsupportedOperationException("Implementiere mich!");
+    }
+}
