728e59fdf78353f0a82b4b4804f09724db41f08e
Olaf Roeder <github@olaf-roeder.de>
2020-02-09 11:25:50 +0100

    Start gelöscht, war redundant mit HelloWorld

--- src/com/hoffrogge/lehreinheit02/HelloWorld.java
+++ src/com/hoffrogge/lehreinheit02/HelloWorld.java
@@ -2,6 +2,7 @@ package com.hoffrogge.lehreinheit02;
 
 public class HelloWorld {
 
+    /* Startpunkt des Programms */
     public static void main(String[] args) {
 
 	System.out.println("Hallo, Welt :)");
--- src/com/hoffrogge/lehreinheit02/Start.java
+++
@@ -1,9 +0,0 @@
-package com.hoffrogge.lehreinheit02;
-
-public class Start {
-
-	public static void main(String[] args) {
-		/* Startpunkt des Programms */
-	}
-
-}
