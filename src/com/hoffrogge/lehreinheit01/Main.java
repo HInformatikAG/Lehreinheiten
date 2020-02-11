346f9f894e4b2977f35d47baa7dcf7b1bc485913
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2020-02-11 10:31:01 +0100

    Source Format

--- src/com/hoffrogge/lehreinheit01/Main.java
+++ src/com/hoffrogge/lehreinheit01/Main.java
@@ -9,30 +9,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-	/*
-	 * =========================================================================
-	 * Dieses Zeichenbrett hat eine Laenge und Breite von 800.
-	 *
-	 * AUFGABE: Schaue dir die Beispiele an und zeichne selbst ein paar Linien.
-	 * Schaffst du es, einen Tetrisspielstein zu zeichnen?
-	 * =========================================================================
-	 */
-	Zeichenbrett zeichenbrett = new Zeichenbrett();
+        /*
+         * =====================================================================
+         * Dieses Zeichenbrett hat eine Laenge und Breite von 800.
+         *
+         * AUFGABE: Schaue dir die Beispiele an und zeichne selbst ein paar
+         * Linien. Schaffst du es, einen Tetrisspielstein zu zeichnen?
+         * =====================================================================
+         */
+        Zeichenbrett zeichenbrett = new Zeichenbrett();
 
-	Dreieck dreieck = new Dreieck(200, 200);
+        Dreieck dreieck = new Dreieck(200, 200);
 
-	GleichseitigesDreieck gleichseitigesDreieck = new GleichseitigesDreieck(300, 300);
-	gleichseitigesDreieck.setFuellFarbe(new Color(123, 123, 123));
+        GleichseitigesDreieck gleichseitigesDreieck = new GleichseitigesDreieck(300, 300);
+        gleichseitigesDreieck.setFuellFarbe(new Color(123, 123, 123));
 
-	zeichenbrett.fuegeZeichenbaresObjektHinzu(dreieck);
-	zeichenbrett.fuegeZeichenbaresObjektHinzu(gleichseitigesDreieck);
+        zeichenbrett.fuegeZeichenbaresObjektHinzu(dreieck);
+        zeichenbrett.fuegeZeichenbaresObjektHinzu(gleichseitigesDreieck);
 
-	zeichenbrett.zeichneAlleZeichenbarenObjekte();
+        zeichenbrett.zeichneAlleZeichenbarenObjekte();
 
-	/* vier Linien, die ein Rechteck ergeben */
-	zeichenbrett.zeichneLinie(50, 750, 200, 750);
-	zeichenbrett.zeichneLinie(200, 750, 200, 650);
-	zeichenbrett.zeichneLinie(200, 650, 50, 650);
-	zeichenbrett.zeichneLinie(50, 650, 50, 750);
+        /* vier Linien, die ein Rechteck ergeben */
+        zeichenbrett.zeichneLinie(50, 750, 200, 750);
+        zeichenbrett.zeichneLinie(200, 750, 200, 650);
+        zeichenbrett.zeichneLinie(200, 650, 50, 650);
+        zeichenbrett.zeichneLinie(50, 650, 50, 750);
     }
 }
