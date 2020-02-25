90c2b45adf169a09ba30794453cda9bd13bbd2a7
Florian <56248466+F-Praktikum@users.noreply.github.com>
2020-02-25 18:30:31 +0100

    [+] Übung: 'Logistische Gleichung'

---
+++ src/com/hoffrogge/uebungen/LogistischeGleichung.java
@@ -0,0 +1,23 @@
+package com.hoffrogge.uebungen;
+
+public class LogistischeGleichung {
+	
+	/*
+	 * Übungsaufgabe 1:
+	 * Implementiere eine Klasse mit einer main-Methode,
+	 * welche Iterationen der logistischen Gleichung berechnet.
+	 * 
+	 * Der Nutzer soll die Variablen r und x angeben und auswählen können,
+	 * wie viele Iterationen berechnet werden sollen.
+	 * 
+	 * Die Eingabe des Nutzers ist auf Sinnigkeit zu überprüfen.
+	 * 
+	 * Formel:
+	 * x    = rx (1-x )
+	 *  n+1     n    n
+	 * 
+	 * Gerenderte Formel (STRG+Click, um den Link zu öffnen):
+	 * https://wikimedia.org/api/rest_v1/media/math/render/svg/0b7e50fedb8d76207192160eac0a83744f92a567
+	 */
+	
+}
