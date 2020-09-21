093ad4070e2a005e52697a658bb5d80a156b9927
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2020-09-21 09:29:52 +0200

    Übung zu LE1 etwas vereinfacht;

--- src/com/hoffrogge/lehreinheit01/Main.java
+++ src/com/hoffrogge/lehreinheit01/Main.java
@@ -1,74 +1,48 @@
 package com.hoffrogge.lehreinheit01;
 
-import java.awt.Color;
-
-import com.hoffrogge.lehreinheit04.Dreieck;
-import com.hoffrogge.lehreinheit04.GleichseitigesDreieck;
-
+/*
+ * Zeichenfläche mit einer Größe von 800 x 800 Pixeln
+ * 
+ *  0,0 ----------------------------------------- 800,0
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  |                                               |
+ *  0,800 --------------------------------------- 800,800
+ */
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	    
+	/*
+	 * =====================================================================
+	 * 
+	 * Dieses Zeichenbrett hat eine Laenge und Breite von 800.
+	 *
+	 * AUFGABE: Schaue dir die Beispiele an und zeichne selbst ein paar Linien.
+	 * Schaffst du es, einen Tetrisspielstein zu zeichnen?
+	 * =====================================================================
+	 */
 
-        /*
-         * =====================================================================
-         * Dieses Zeichenbrett hat eine Laenge und Breite von 800.
-         *
-         * AUFGABE: Schaue dir die Beispiele an und zeichne selbst ein paar
-         * Linien. Schaffst du es, einen Tetrisspielstein zu zeichnen?
-         * =====================================================================
-         */
-	
-        /*
-         * Zeichenflaeche mit einer Groesse von 800 x 800 Pixeln
-         *
-         *  x,y Koordinaten
-         *
-         *  0,0 ----------------------------------------- 800,0
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  |                                               |
-         *  0,800 --------------------------------------- 800,800
-         *
-         */
-        
-        Zeichenbrett zeichenbrett = new Zeichenbrett();
+	Zeichenbrett zeichenbrett = new Zeichenbrett();
 
-		Dreieck dreieck = new Dreieck(200, 200);
-
-		GleichseitigesDreieck gleichseitigesDreieck = new GleichseitigesDreieck(300, 300);
-		gleichseitigesDreieck.setFuellFarbe(new Color(123, 123, 123));
-
-		// zeichenbrett.fuegeZeichenbaresObjektHinzu(dreieck);
-		// zeichenbrett.fuegeZeichenbaresObjektHinzu(gleichseitigesDreieck);
-
-		zeichenbrett.zeichneAlleZeichenbarenObjekte();
-
-		/* vier Linien, die ein Rechteck ergeben */
-		// zeichenbrett.zeichneLinie(50, 750, 200, 750);
-		// zeichenbrett.zeichneLinie(200, 750, 200, 650);
-		// zeichenbrett.zeichneLinie(200, 650, 50, 650);
-		// zeichenbrett.zeichneLinie(50, 650, 50, 750);
-
-		for (int x = 0; x < 900; x += 10)
-			for (int y = 0; y < 900; y += 10)
-				for (int x2 = 0; x2 < 900; x2 += 10)
-					for (int y2 = 0; y2 < 900; y2 += 100)
-						zeichenbrett.zeichneLinie(x, y, x2, y2);
-	}
+	/* vier Linien, die ein Rechteck ergeben */
+	zeichenbrett.zeichneLinie(50, 750, 200, 750);
+	zeichenbrett.zeichneLinie(200, 750, 200, 650);
+	zeichenbrett.zeichneLinie(200, 650, 50, 650);
+	zeichenbrett.zeichneLinie(50, 650, 50, 750);
+    }
 }
